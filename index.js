7fec1e2d20ef8282db7414c6bfa1b92bbaa73330
NikhiSagar <130742583+NikhiSagar@users.noreply.github.com>
2023-04-14 07:28:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,11 @@ exports.execute = async function (event, context) {
   // Get the input arguments from the journey
   const inputArgs = event.inArguments[0];
 
-  // Set up the options for the HTTP request
-  const options = {
-    hostname: 'https://nikhilwebhook.herokuapp.com/',
-    path: '/path/to/webhook',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  // Do some processing with the input arguments
+  const processedData = inputArgs.data.toUpperCase();
 
-  // Create a new request object
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
-    res.on('data', (d) => {
-      process.stdout.write(d);
-    });
-  });
-
-  // Handle errors
-  req.on('error', (error) => {
-    console.error(error);
-  });
-
-  // Send the payload to the webhook
-  req.write(JSON.stringify(inputArgs));
-  req.end();
+  // Save the processed data to a database or external service
+  // ...
 
   // Return the result of the activity execution
   return { status: 'ok' };
